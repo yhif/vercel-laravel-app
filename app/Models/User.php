e441d2c91570d0fe51099740e708a8dbad735929
Coke <daiyihui0520@gmail.com>
2024-06-16 22:44:58 +0800

    Update

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@example.com') && $this->hasVerifiedEmail();
+        // return str_ends_with($this->email, '@example.com') && $this->hasVerifiedEmail();
+        return true;
     }
 }
